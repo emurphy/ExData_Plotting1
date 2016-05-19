5bebd2f415820a003db242c608fa14ba2445a80f
Emmet Murphy <emmet@emurphy.net>
2016-05-18 23:45:38 -0700

    plot1 to screen

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+library(data.table)
+
 # Dowload and unzip dataset, unless the unzipped file already exists
 if (!file.exists("household_power_consumption.txt")) {
     fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
@@ -5,4 +7,9 @@ if (!file.exists("household_power_consumption.txt")) {
     unzip("exdata-data-household_power_consumption.zip")
 }
 
+df <- fread("household_power_consumption.txt", na.strings="?")
+df$Date = as.Date(df$Date, "%d/%m/%Y")
 
+df <- df[df$Date == "2007-02-01" | df$Date == "2007-02-02",]
+
+with(df, hist(Global_active_power, col="red"))
